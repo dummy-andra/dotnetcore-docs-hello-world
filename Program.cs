5adc146806db124355aaa4876112cb2f67aacd52
Andra Gabriela Radu <37038210+dummy-andra@users.noreply.github.com>
2023-07-21 19:27:21 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 public class Program
@@ -10,11 +11,44 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+            })
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
             });
 }
+
+
+
+
+
+
+
+
+
+
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.Extensions.Hosting;
+
+// public class Program
+// {
+//     public static void Main(string[] args)
+//     {
+//         CreateHostBuilder(args).Build().Run();
+//     }
+
+//     public static IHostBuilder CreateHostBuilder(string[] args) =>
+//         Host.CreateDefaultBuilder(args)
+//             .ConfigureWebHostDefaults(webBuilder =>
+//             {
+//                 webBuilder.UseStartup<Startup>();
+//             });
+// }
 
 
 
