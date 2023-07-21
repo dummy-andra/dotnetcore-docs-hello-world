5548542d27f99f8f25cbdcf5c9b2aca00eb6adad
Andra Gabriela Radu <37038210+dummy-andra@users.noreply.github.com>
2023-07-21 15:25:41 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,25 +1,76 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.IO;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Access the Azure Web App Application Settings (environment_stage)
+        var environmentValue = builder.Configuration["environment_stage"];
+
+        // Use the environmentValue as needed in your application
+        // For example, you can use it to set the environment or change app settings files.
+
+        // Write the environmentValue to a text file
+        var filePath = "environmentValue.txt";
+        File.WriteAllText(filePath, $"Here is the variable value: {environmentValue}");
+
+        builder.Services.AddRazorPages();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
 
-app.UseAuthorization();
 
-app.MapRazorPages();
 
-app.Run();
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+// builder.Services.AddRazorPages();
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
+
+// app.UseHttpsRedirection();
+// app.UseStaticFiles();
+
+// app.UseRouting();
+
+// app.UseAuthorization();
+
+// app.MapRazorPages();
+
+// app.Run();
