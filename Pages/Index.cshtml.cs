747c607718085da43cfb1ce5ed39f9d87d528306
Andra Gabriela Radu <37038210+dummy-andra@users.noreply.github.com>
2023-07-21 19:39:19 +0300

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,22 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using System.Runtime.InteropServices;
 
-namespace dotnetcoresample.Pages;
-
-public class IndexModel : PageModel
+namespace dotnetcoresample.Pages
 {
-
-    public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
-    
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly IConfiguration _configuration;
+        public string OSVersion { get { return RuntimeInformation.OSDescription; } }
 
-    public void OnGet()
-    {        
+        public string Message { get; private set; }
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            // Read the value of environment_stage from the configuration
+            var environmentStage = _configuration["environment_stage"];
+
+            // Check if the environment_stage is "SIT" and set the message accordingly
+            if (environmentStage == "SIT")
+            {
+                Message = "Andra is cool 💜";
+            }
+            else
+            {
+                Message = ".NET 💜 Azure";
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+// ﻿using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Mvc.RazorPages;
+// using System.Runtime.InteropServices;
+
+// namespace dotnetcoresample.Pages;
+
+// public class IndexModel : PageModel
+// {
+
+//     public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
+    
+//     private readonly ILogger<IndexModel> _logger;
+
+//     public IndexModel(ILogger<IndexModel> logger)
+//     {
+//         _logger = logger;
+//     }
+
+//     public void OnGet()
+//     {        
+//     }
+// }
