df1ed3ff02662254f40e43a72f1716a8bd353866
Andra Gabriela Radu <37038210+dummy-andra@users.noreply.github.com>
2023-07-21 19:00:07 +0300

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -41,6 +41,7 @@ public class Startup
         // services.Configure<MyOptions>(Configuration.GetSection("MyOptions"));
 
         // Register other services here...
+        services.AddRazorPages();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,30 +57,16 @@ public class Startup
             app.UseHsts();
         }
 
-        var builder = WebApplication.CreateBuilder(args);
-        
-        // Add services to the container.
-        builder.Services.AddRazorPages();
-        
-        var app = builder.Build();
-        
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-        
         app.UseHttpsRedirection();
         app.UseStaticFiles();
-        
+
         app.UseRouting();
-        
+
         app.UseAuthorization();
-        
-        app.MapRazorPages();
-        
-        app.Run();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapRazorPages();
+        });
     }
 }
