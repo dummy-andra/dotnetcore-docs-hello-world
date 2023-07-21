d8d35177851812a7d2e9ab529d97130744330310
Andra Gabriela Radu <37038210+dummy-andra@users.noreply.github.com>
2023-07-21 16:05:29 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,37 +1,57 @@
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
-namespace YourWebApiNamespace
+class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) =>
+        var host = new HostBuilder()
+            .ConfigureAppConfiguration((hostContext, config) =>
+            {
+                var settings = config.Build();
+                var connection = settings["ConnectionStrings:AppConfig"];
+                config.AddAzureAppConfiguration(options =>
                 {
-                    var settings = config.Build();
-                    config.AddAzureAppConfiguration(options =>
-                    {
-                        // Connection string to the Azure App Configuration service.
-                        options.Connect(settings["ConnectionStrings:AppConfig"])
-                               .ConfigureKeyVault(kv =>
-                               {
-                                   kv.SetCredential(new DefaultAzureCredential());
-                               });
-                    });
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
+                    options.Connect(connection)
+                           .UseFeatureFlags();
                 });
+
+                // Retrieve the "environment_stage" variable from Azure App Configuration
+                var environmentStage = settings["environment_stage"];
+
+                // Create a new configuration builder for SIT environment
+                var configurationBuilder = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.SIT.json", optional: true, reloadOnChange: true);
+
+                // Set the "environment_stage" value in the new configuration builder
+                configurationBuilder.AddInMemoryCollection(new[]
+                {
+                    new KeyValuePair<string, string>("environment_stage", environmentStage)
+                });
+
+                // Replace the original configuration with the new one that contains "environment_stage"
+                config.AddConfiguration(configurationBuilder.Build());
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                // Add your services here, if any
+            })
+            .Build();
+
+        // Now you can access configuration values using the host's Configuration property
+        var settingValue = host.Services.GetRequiredService<IConfiguration>()["SettingKey"];
+
+        Console.WriteLine($"SettingValue: {settingValue}");
+
+        // Run any other logic or services you need here
+
+        host.Run();
     }
 }
+
 
 
 
